0c98011eacc3c3e98dfc86f7af49d8ca2327ff82
Geoff-Goodwin-Dev <ggoodwin.dev@gmail.com>
2018-08-16 17:21:17 -0400

    adds content

--- assets/javascript/script.js
+++
@@ -1,9 +0,0 @@
-const links = {
-  linkedIn: 'https://www.linkedin.com/in/geofflgoodwin/',
-  github: 'https://github.com/Geoff-Goodwin-Dev'
-};
-
-document.querySelector("#linkToSocialMedia").innerHTML =`<ul id="listOfLinks"></ul>`;
-let listItem = document.createElement('LI');
-document.querySelector("#listOfLinks").innerHTML = `<li><a href="${links.linkedIn}" target="_blank">linkedIn</a></li>`;
-document.querySelector("#listOfLinks").appendChild(listItem);
